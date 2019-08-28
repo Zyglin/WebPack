bcd2318de3d57bda6c4c44c8b3ca4182748d3756
Zyglin <zyglin@mail.ru>
2019-08-28 15:05:32 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React, { lazy, Suspense } from "react";
+import React  from "react";
 import ReactDOM from "react-dom";
 
-const App = lazy(() => import("./component/app.component"));
 ReactDOM.render(
-  <Suspense fallback={<p>loading....</p>}>
-    <App />
-  </Suspense>,
-  document.querySelector("#root")
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
 );
+
